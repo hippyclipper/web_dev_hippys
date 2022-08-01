d86cf7e5821df322e783936e2a3ce36a16ebed2f
hippyclipper <cameron_moon321@hotmail.com>
2022-08-01 15:41:42 -0700

    text wrapping for questions

---
+++ quiz/script.js
@@ -0,0 +1,17 @@
+var list_a = document.getElementById("list_a");
+var list_b = document.getElementById("list_b");
+var list_c = document.getElementById("list_c");
+var list_d = document.getElementById("list_d");
+
+function loadQuiz() {
+
+    list_a.innerText = "hereherehereherehereherehereheherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehererehereherehereherehereherehereherehere";
+    list_b.innerText = "here";
+    list_c.innerText = "here";
+    list_d.innerText = "here";
+
+}
+
+
+
+loadQuiz();
